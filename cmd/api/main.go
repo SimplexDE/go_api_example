a80daec64fa025096f01a646eb2a5e409f50cc4c
Simplex <kontakt.simplex@pm.me>
2024-10-29 16:28:01 +0100

    better

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,27 +3,31 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"log"
 
 	"github.com/go-chi/chi"
 	"github.com/SimplexDE/go_api_example/internal/handlers"
-	log "github.com/sirupsen/logrus"
+	logg "github.com/sirupsen/logrus"
 )
 
 func main() {
 
-	log.SetReportCaller(true)
+	logg.SetReportCaller(true)
 	var r *chi.Mux = chi.NewRouter()
 	handlers.Handler(r)
+	fmt.Println(`
+   __________     ___    ____  ____
+  / ____/ __ \   /   |  / __ \/  _/
+ / / __/ / / /  / /| | / /_/ // /  
+/ /_/ / /_/ /  / ___ |/ ____// /   
+\____/\____/  /_/  |_/_/   /___/   `)
 
-	fmt.Println("Starting GO API service...")
-
-	fmt.Println(`Cooler Print Statement`)
+	fmt.Println(" ")
+	log.Println("Starting GO API service...")
 
 	err := http.ListenAndServe("localhost:8000", r)
 
 	if err != nil {
-		log.Error(err)
+		logg.Error(err)
 	}
-
-
 }
